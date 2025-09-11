089f6a186e80756f469483e80a049c2c01e09652
Aviral Saxena <aviralsaxena2006@gmail.com>
2025-09-11 23:54:34 +0530

    Added basic cors setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,24 @@
 # In backend/app/main.py
 
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# Defiing a route for the root URL "/"
+# Define the origins that are allowed to make requests
+origins = [
+    "http://localhost:3000", # The origin for your Next.js frontend
+]
+
+# Add the CORS middleware to your application
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"], # Allows all headers
+)
+
 @app.get("/")
 def read_root():
     return {"message": "Backend is running!"}
-
-
