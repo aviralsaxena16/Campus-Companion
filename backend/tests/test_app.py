1c482047a0748a73bf269c0404996fccd097607f
Aviral Saxena <aviralsaxena2006@gmail.com>
2026-03-14 20:31:47 +0530

    Updated the endpoints

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -7,11 +7,3 @@ def test_root_endpoint():
     res = client.get("/")
     assert res.status_code == 200
 
-def test_predict_dummy():
-    payload = {
-        "subject": "Career Fair",
-        "body": "There is a hiring drive on campus"
-    }
-    res = client.post("/api/predict", json=payload)
-    assert res.status_code == 200
-    assert "label" in res.json()
